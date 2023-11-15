d03488d97a143f11c56979602c8aa61c52bc41c9
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-11-15 10:06:24 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace GitSearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
