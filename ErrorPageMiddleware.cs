734e559c68dd3fc26e2cf7b8264d4d922af49182
Zakhar Merkurev <merkurevzahar@gmail.com>
2023-11-15 13:07:07 +0800

    добавление страницы ошибки и базовой страницы

---
+++ ErrorPageMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace GitSearch
+{
+    public class ErrorPageMiddleware
+    {
+        private RequestDelegate _next;
+
+        public ErrorPageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.ContentType = "text/html; charset=utf-8";
+
+            await context.Response.SendFileAsync("C:..\\GitSearch\\wwwroot\\html\\error-page.html");
+        }
+    }
+}
---
+++ MainPageMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace GitSearch
+{
+    public class MainPageMiddleware
+    {
+        private RequestDelegate _next;
+
+        public MainPageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(context.Request.Path == "/")
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+
+                await context.Response.SendFileAsync("C:..\\GitSearch\\wwwroot\\index.html");
+            }
+            else
+            {
+                await _next.Invoke(context);    
+            }
+        }
+    }
+}
